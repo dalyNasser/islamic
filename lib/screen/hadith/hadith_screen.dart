5c785aa0a52c966cf4bf4d96f8fa666788bca03e
dell <dalianasser313@gmail.com>
2025-07-28 00:42:21 +0300

    finish app2

--- lib/screen/hadith/hadith_screen.dart
+++ lib/screen/hadith/hadith_screen.dart
@@ -3,8 +3,7 @@ import 'dart:developer';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:islamic/core/app_asset.dart';
-import 'package:islamic/core/app_color.dart';
+import 'package:islamic/screen/hadith/hadith_card.dart';
 
 class HadithScreen extends StatefulWidget {
   const HadithScreen({super.key});
@@ -25,56 +24,7 @@ class _HadithScreenState extends State<HadithScreen> {
 
             itemCount: hadithes.length,
               itemBuilder: (context,i,pageIndex) {
-                return Container(
-                  width: 313,
-                  height: 818,
-                  decoration: BoxDecoration(color: AppColor.fontColor,borderRadius: BorderRadius.circular(16)),
-                  child: Stack(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [Image.asset(AppAsset.left_corner,color: Colors.black,),
-                          Image.asset(AppAsset.right_corner,color: Colors.black,)],),
-                      ),
-                      Positioned(
-                          child: Opacity(
-
-                        opacity: 0.25,
-                        child: Image.asset(
-                          AppAsset.onboardinQuran,
-                          fit: BoxFit.contain,
-                          color: Colors.black,
-                          width: 313,
-                          height: 818,
-                        ),
-                      )),
-                      Positioned(
-                        top:10,
-                        right: 10,
-                        left:10,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: SizedBox(
-                            width: 200,
-                            height: 350,
-                            child: ListView(
-                              // mainAxisAlignment: MainAxisAlignment.center,
-                              // crossAxisAlignment: CrossAxisAlignment.stretch,
-                              children: [
-                                SizedBox(child:Text(textAlign: TextAlign.center,hadithes[i],style: Theme.of(context).textTheme.bodyMedium?.copyWith(color: Colors.black,height: 1.8),)
-                                                      ),
-                              ],
-                            ),
-                          ),
-                        ),),
-
-                      Positioned(bottom:0,child: Image.asset(AppAsset.img_bottom_decoration,color: Colors.black,fit: BoxFit.contain,),),
-
-                    ],
-                  ),
-    );},
+                return hadith_card(i, context);},
               options: CarouselOptions(
                 onPageChanged: (x,_)async{
                   await getHadith(x);
@@ -101,30 +51,20 @@ class _HadithScreenState extends State<HadithScreen> {
       ],
     );
   }
-
-
 Future<void> getHadith(int x)async{
     hadithe=hadithes[x].split('\n');
     setState(() {
 
     });
-
-
-
-
-
     }
-
 Future<void> loadHadith()async{
 
-    for(int i=1;i<=50;i++){
+    for(int i=1;i<=hadithes.length;i++){
       String content=await rootBundle.loadString('assets/text/hadeeth/h$i.txt');
-      log(content);
       hadithes.add(content);
     }
    await getHadith(0);
     setState(() {
     });
-
 }
 }
